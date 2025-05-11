1918c53ae958fab3b69dae9ed0786b5e16040363
Project <jmichael5@student.cvtc.edu>
2025-05-11 13:10:42 -0500

    Added spike trap

---
+++ Assets/Scripts/TrapScripts/SpikeTrap.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class SpikeTrap : MonoBehaviour
+{
+    public int damage = 10;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerInformation.control.TakeDamage(damage);
+        }
+    }
+}
