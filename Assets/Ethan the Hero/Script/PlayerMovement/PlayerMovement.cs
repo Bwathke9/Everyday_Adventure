61cb0f698c24653e49cf79522acab275f221bd53
Project <jmichael5@student.cvtc.edu>
2025-05-17 22:03:49 -0500

    Fix high jump script

--- Assets/Ethan the Hero/Script/PlayerMovement/PlayerMovement.cs
+++ Assets/Ethan the Hero/Script/PlayerMovement/PlayerMovement.cs
@@ -214,7 +214,7 @@ namespace EthanTheHero
             if (jumpButtonPressed && grounded)
             {
                 isJumping = true;
-                myBody.linearVelocity = new Vector2(myBody.linearVelocity.x, data.jumpHeight);
+                myBody.linearVelocity = new Vector2(myBody.linearVelocity.x, jumpForce);
             }
         }
         #endregion
