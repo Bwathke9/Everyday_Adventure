877ba4f9c47c36827425457877df74658a2002a7
Adam <anixdorf@student.cvtc.edu>
2025-05-19 19:13:58 -0500

    fix spelling on start screen

--- Assets/Scripts/StartDisplay.cs
+++ Assets/Scripts/StartDisplay.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class StartDisplay : MonoBehaviour
 {
     private string startMessage = "Welcome to EVERYDAY ADVENTURE! \n To travel through the levels with Ethan use " +
-        "D to move right, A to move left, and SPACE to jump. Ethan has Ninja abilities and can jump at times" +
+        "D to move right, A to move left, and SPACE to jump. Ethan has Ninja abilities and can jump at times " +
         "when he is hanging onto the edge. Collect fruit to increase your score but " +
         "watch out for obstacles along the way. Watch for hearts, they will give you a chance for you to " +
         "restore your health. Levels 1&2 will have a mini game at the end for a chance to earn Bonus points. " +
